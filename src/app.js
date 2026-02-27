e92c67e302241970ef6bb2a3c24e2da27a0b5146
GabrielTrindade <gabrieldiastrin63@gmail.com>
2026-02-27 15:57:40 -0300

    Adicionando cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
 
 const usersRoutes = require("./routes/user_routes");
 
 app.use(express.json());
+app.use(cors());
 app.use("/api", usersRoutes);
 
 const PORT = 3001;
